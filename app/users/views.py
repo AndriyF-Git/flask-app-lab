04ab77f8fe1271ede96d0d5f0dd409c16b899bf2
Andriy Fedirko <fedirko816@gmail.com>
2024-10-26 16:04:23 +0300

    Moved user routes to users folder, and tested

--- app/users/views.py
+++ app/users/views.py
@@ -1,20 +1,14 @@
+from . import user_bp
 from flask import request, redirect, url_for, render_template, abort
-from . import app
 
-@app.route('/')
+@user_bp.route('/')
 def main():
     return render_template("base.html")
 
-@app.route('/homepage') 
-def home():
-    """View for the Home page of your website."""
-    agent = request.user_agent
-
-    return render_template("home.html", agent=agent)
 
 #users
 
-@app.route("/hi/<string:name>")   #/hi/ivan?age=45
+@user_bp.route("/hi/<string:name>")   #/hi/ivan?age=45
 def greetings(name):
     name = name.upper()
     age = request.args.get("age", None, int)   
@@ -22,8 +16,15 @@ def greetings(name):
     return render_template("hi.html", 
                            name=name, age=age)
 
-@app.route("/admin")
+@user_bp.route("/admin")
 def admin():
-    to_url = url_for("greetings", name="administrator", age=45, _external=True)     # "http://localhost:8080/hi/administrator?age=45"
+    to_url = url_for("users.greetings", name="administrator", age=45, _external=True)     # "http://localhost:8080/hi/administrator?age=45"
     print(to_url)
     return redirect(to_url)
+
+@user_bp.route('/homepage')
+def home():
+    """View for the Home page of your website."""
+    agent = request.user_agent
+
+    return render_template("home.html", agent=agent)
