5ef6a3b5dcffc5422974552644453b58ba33cac5
Andriy Fedirko <fedirko816@gmail.com>
2024-11-02 23:08:23 +0200

    Fixed!!!!!!!!!!!!!!!!! And add cookies, variable theme, update navigation bar, add possibility add/remove cookies

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from . import app
 
 @app.route('/')
 def main():
-    return render_template("base.html")
+    return render_template("index.html")
 
 @app.route('/homepage') 
 def home():
@@ -11,5 +11,10 @@ def home():
     agent = request.user_agent
 
     return render_template("home.html", agent=agent)
+
+@app.route('/resume')
+def show_resume():
+    return render_template("resume.html")
+
 
 
